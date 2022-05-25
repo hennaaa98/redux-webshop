827556511128c7912f9943c2c194a73f8e496fff
Henna <qureshi.henna@gmail.com>
2022-05-25 17:55:57 +0200

    dist

---
+++ precache-manifest.36e9607f3f4d112fa54d83302fd77d94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ace8d1bc108705e3cfc829ff6cdc394",
+    "url": "/redux-webshop/index.html"
+  },
+  {
+    "revision": "c4035b5b927a0bcbafd6",
+    "url": "/redux-webshop/static/css/main.31d53345.chunk.css"
+  },
+  {
+    "revision": "6b7850e4565da57c4d5f",
+    "url": "/redux-webshop/static/js/2.3429789b.chunk.js"
+  },
+  {
+    "revision": "e862cea9dad34730b7f65f6ad78008cf",
+    "url": "/redux-webshop/static/js/2.3429789b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4035b5b927a0bcbafd6",
+    "url": "/redux-webshop/static/js/main.79536010.chunk.js"
+  },
+  {
+    "revision": "f20ef25a6f67907a0ae4",
+    "url": "/redux-webshop/static/js/runtime-main.004ce71c.js"
+  }
+]);
